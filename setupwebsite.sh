def50a269ffa05fe012ba58ad55bbc4b96500ce1
sujatce <70561546+sujatce@users.noreply.github.com>
2021-10-06 19:31:42 -0700

    Update setupwebsite.sh

--- setupwebsite.sh
+++ setupwebsite.sh
@@ -4,7 +4,7 @@ echo -e "\nFollowing command installs git\n"
 sudo apt-get install git -y
 
 cd /var/www/html/
-sudo git clone https://"sujatce":"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"@github.com/sujatce/csun
+sudo git clone https://"sujatce":"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"@github.com/sujatce/csun
 
 printf "
 RewriteEngine On
